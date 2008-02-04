efdddf4134ab63420dca7d385b4e9657c782d69c
Justin Forest <justin.forest@gmail.com>
2008-02-04 14:02:43 +0000

    Интерфейс СУК недоступен анонимным пользоателям.

--- themes/admin/templates/page.default.php
+++ themes/admin/templates/page.default.php
@@ -2,7 +2,9 @@
 
 require_once(dirname(__FILE__) .'/functions.inc');
 
-$user = AuthCore::getInstance()->getUser();
+_admin_check_perm();
+
+$user = mcms::user();
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
