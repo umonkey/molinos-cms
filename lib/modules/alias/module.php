76f5cf07b0e225a459295a26c459435114b1dd7b
Justin Forest <justin.forest@gmail.com>
2008-02-08 14:18:03 +0000

    Инсталляция таблицы node__alias.

--- lib/modules/alias/module.php
+++ lib/modules/alias/module.php
@@ -5,8 +5,28 @@ class AliasModule implements iRequestHook
   public static function hookRequest(RequestContext $ctx = null)
   {
     if (null === $ctx) {
-      if (null !== ($next = mcms::db()->getResult("SELECT `dst` FROM `node__path` WHERE `src` = :src", array(':src' => $_SERVER['REQUEST_URI']))))
+      if (null !== ($next = mcms::db()->getResult("SELECT `dst` FROM `node__alias` WHERE `src` = :src", array(':src' => $_SERVER['REQUEST_URI']))))
         bebop_redirect($next, 301);
+    }
+  }
+
+  public static function hookPostInstall()
+  {
+    $t = new TableInfo('node__alias');
+
+    if (!$t->exists()) {
+      $t->columnSet('src', array(
+        'type' => 'varchar(255)',
+        'required' => true,
+        'key' => 'mul',
+        ));
+      $t->columnSet('dst', array(
+        'type' => 'varchar(255)',
+        'required' => true,
+        'key' => 'uni',
+        ));
+
+      $t->commit();
     }
   }
 };
