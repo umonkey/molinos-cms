a1cee7d4b8e3a4627d51517fadcce9e437862636
Justin Forest <justin.forest@gmail.com>
2008-02-09 14:04:06 +0000

    Объединение типов документов в mod_basetypes.

---
+++ lib/modules/basetypes/module.php
@@ -0,0 +1,15 @@
+<?php
+
+define('NODEBASE_PATH', dirname(__FILE__) .'/');
+
+require_once(NODEBASE_PATH .'node-access-control.inc');
+require_once(NODEBASE_PATH .'node-query-builder.inc');
+require_once(NODEBASE_PATH .'node.inc');
+require_once(NODEBASE_PATH .'node-base.inc');
+require_once(NODEBASE_PATH .'node-domain.inc');
+require_once(NODEBASE_PATH .'node-file.inc');
+require_once(NODEBASE_PATH .'node-group.inc');
+require_once(NODEBASE_PATH .'node-tag.inc');
+require_once(NODEBASE_PATH .'node-type.inc');
+require_once(NODEBASE_PATH .'node-user.inc');
+require_once(NODEBASE_PATH .'node-widget.inc');
