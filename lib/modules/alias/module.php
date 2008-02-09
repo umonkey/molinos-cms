2a58c42314e370b1d857d42ad55e4c8f8cd14092
Justin Forest <justin.forest@gmail.com>
2008-02-09 13:13:27 +0000

    mod_alias не падает, если таблица не создана.

--- lib/modules/alias/module.php
+++ lib/modules/alias/module.php
@@ -5,8 +5,11 @@ class AliasModule implements iRequestHook
   public static function hookRequest(RequestContext $ctx = null)
   {
     if (null === $ctx) {
-      if (null !== ($next = mcms::db()->getResult("SELECT `dst` FROM `node__alias` WHERE `src` = :src", array(':src' => $_SERVER['REQUEST_URI']))))
-        bebop_redirect($next, 301);
+      try {
+        if (null !== ($next = mcms::db()->getResult("SELECT `dst` FROM `node__alias` WHERE `src` = :src", array(':src' => $_SERVER['REQUEST_URI']))))
+          bebop_redirect($next, 301);
+      } catch (PDOException $e) {
+      }
     }
   }
 
