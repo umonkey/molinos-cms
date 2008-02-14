5a620b5e1d8200c80c3f38c5bd786ec2d03f707e
Justin Forest <justin.forest@gmail.com>
2008-02-14 10:43:12 +0000

    mod_maintenance

---
+++ lib/modules/maintenance/module.php
@@ -0,0 +1,61 @@
+<?php
+// vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
+
+class MaintenanceModule implements iModuleConfig, iRequestHook
+{
+  public static function formGetModuleConfig()
+  {
+    $form = new Form(array(
+      'title' => t('Управление техническими работами'),
+      ));
+
+    $form->addControl(new EnumRadioControl(array(
+      'value' => 'config_state',
+      'label' => t('Текущее состояние'),
+      'options' => array(
+        '' => t('Сайт работает'),
+        'closed' => t('Ведутся технические работы'),
+        ),
+      )));
+
+    return $form;
+  }
+
+  public static function hookPostInstall()
+  {
+  }
+
+  public static function hookRequest(RequestContext $ctx = null)
+  {
+    if (null === $ctx) {
+      $conf = mcms::modconf('maintenance');
+
+      if (!empty($conf['state']) and 'closed' === $conf['state']) {
+        $url = bebop_split_url();
+
+        if ('/admin/' != substr($url['path'], 0, 7)) {
+          header('HTTP/1.1 503 Service Unavailable');
+          header('Content-Type: text/plain; charset=utf-8');
+          die(t('На сервере ведутся технические работы, обратитесь чуть позже.'));
+        }
+      }
+    }
+  }
+
+  public static function getDashboardIcons()
+  {
+    $icons = array();
+    $user = mcms::user();
+
+    if ($user->hasGroup('Developers') or $user->hasGroup('Site Managers'))
+      $icons[] = array(
+        'group' => 'Developement',
+        'img' => 'img/dashboard.gif',
+        'href' => '/admin/builder/modules/?BebopModules.edit=maintenance&destination='. urlencode($_SERVER['REQUEST_URI']),
+        'title' => t('Технические работы'),
+        'description' => t('Позволяет временно закрыть сайт для проведения технических работ.'),
+        );
+
+    return $icons;
+  }
+}
