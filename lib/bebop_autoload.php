166b7aca453563b9f851ef1da7bf3807fa9492ee
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-03-21 15:20:59 +0000

    Обработка отсутствия прав на файл с классом.

--- lib/bebop_autoload.php
+++ lib/bebop_autoload.php
@@ -10,14 +10,17 @@ function bebop_autoload($class_name)
     $k = strtolower($class_name);
 
     if (array_key_exists($k, $map)) {
+      if (!is_readable($map[$k]))
+        mcms::fatal("{$class_name} is in a file which is read-protected: {$map[$k]}");
+
       include($map[$k]);
 
       $isif = (substr($class_name, 0, 1) === 'i');
 
       if ($isif and !in_array($class_name, get_declared_interfaces()))
-        mcms::fatal("There is no {$class_name} interface in {$map[$k]}");
+        mcms::fatal("There is no {$class_name} interface in {$map[$k]}", array('declared_interfaces' => get_declared_interfaces()));
       elseif (!$isif and !in_array($class_name, get_declared_classes()))
-        mcms::fatal("There is no {$class_name} class in {$map[$k]}");
+        mcms::fatal("There is no {$class_name} class in {$map[$k]}", array('declared_classes' => get_declared_classes()));
     }
 
     else {
