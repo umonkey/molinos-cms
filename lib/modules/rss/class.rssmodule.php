d6239af4056b2aadb6be692910a5ec67e1ed9f08
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-03-25 10:50:26 +0000

    Удалены лишние методы.

--- lib/modules/rss/class.rssmodule.php
+++ lib/modules/rss/class.rssmodule.php
@@ -21,56 +21,6 @@ class RSSModule implements iRemoteCall, iAdminMenu, iAdminUI
     die($xml);
   }
 
-  private static function getItems(Node $feed)
-  {
-    $filter = array(
-      'published' => 1,
-      'class' => preg_split('/, */', $feed->types),
-      );
-
-    foreach (explode(' ', $feed->sort) as $field) {
-      if (substr($field, 0, 1) == '-') {
-        $mode = 'desc';
-        $field = substr($field, 1);
-      } else {
-        $mode = 'asc';
-      }
-
-      $filter['#sort'][$field] = $mode;
-    }
-
-    return Node::find($filter, $feed->limit);
-  }
-
-  private static function formatFeed(Node $feed, array $items)
-  {
-    $output = '<?xml version="1.0" encoding="utf-8" ?>';
-    $output .= '<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">';
-    $output .= '<channel>';
-    $output .= '<title>'. mcms_plain($node->title) .'</title>';
-    $output .= '<description>'. mcms_plain($node->description) .'</description>';
-
-    if (isset($feed->link)) {
-      $output .= '<link>'. $feed->link .'</link>';
-      $output .= '<atom:link href=\'http://'. $_SERVER['HTTP_HOST'] .'/rss.rpc?feed='. $feed->name .'\' rel=\'self\' type=\'application/rss+xml\' />';
-    }
-
-    if (isset($feed->description))
-      $output .= '<description>'. mcms_plain($feed->description) .'</description>';
-
-    if (isset($feed->language))
-      $output .= '<language>'. $feed->language .'</language>';
-
-    $output .= '<pubDate>'. date('r', strtotime($items[key($items)]->created)) .'</pubDate>';
-    $output .= '<generator>http://molinos-cms.googlecode.com/</generator>';
-
-    $output .= self::formatItems($items);
-
-    $output .= '</channel></rss>';
-
-    return $output;
-  }
-
   public static function getMenuIcons()
   {
     $icons = array();
