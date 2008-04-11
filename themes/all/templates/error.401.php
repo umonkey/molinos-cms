fdb8d30cd72d2ac7abaaab450db686c12ad76ad4
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-04-11 16:33:56 +0000

    Новая форма входа.

--- themes/all/templates/error.401.php
+++ themes/all/templates/error.401.php
@@ -1,44 +1,75 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+
+$tmp = bebop_split_url();
+$tmp['args']['login'] = 'error';
+
+$laction = bebop_combine_url(array(
+  'path' => '/base.rpc',
+  'args' => array(
+    'action' => 'login',
+    'onerror' => bebop_combine_url($tmp, false),
+    'destination' => $_SERVER['REQUEST_URI'],
+    ),
+  ));
+
+// Ссылка на восстановление пароля (?login=error).
+$lforgot = bebop_combine_url($tmp);
+
+// Ссылка на вход (первоначальная).
+$tmp['args']['login'] = null;
+$lreturn = bebop_combine_url($tmp);
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <head>
-    <title><?=$error['message']?></title>
-    <link rel='stylesheet' type='text/css' href='<?=$prefix?>/errors.css' />
+    <title><?php print $_SERVER['HTTP_HOST']; ?> — авторизация</title>
+    <meta http-equiv='Content-Type' content='application/xhtml+xml; charset=UTF-8' />
+    <meta name='robots' content='index,follow' />
+    <link rel='shortcut icon' href='/themes/all/img/lock.png' type='image/png' />
+    <link rel='stylesheet' href='/themes/all/401.css' type='text/css' />
+    <link rel='stylesheet' href='/themes/all/refpoint.reset.css' type='text/css' />
+    <link rel='stylesheet' href='/themes/all/refpoint.typography-16.css' type='text/css' />
+    <script type='text/javascript' language='javascript' src='/themes/all/401.js'></script>
+    <script type='text/javascript' language='javascript'>function show_restore() { document.getElementById('user-login-form').style.display = 'none'; document.getElementById('profile-remind-form').style.display = 'block'; return false; } function show_login() { document.getElementById('user-login-form').style.display = 'block'; document.getElementById('profile-remind-form').style.display = 'none'; return false; }</script>
   </head>
   <body>
-    <h1><a href="http://code.google.com/p/molinos-cms/"><span><?=$error['message']?></span></a></h1>
-    <p class='main'><?=$error['description']?></p>
-    <p><?php
-      if (!isset($_GET['profile_status']) or 'wrong' !== $_GET['profile_status'])
-        print $error['note'];
-      else
-        print 'Введённый логин/пароль не подходит.  Возможно, ваш профиль ещё не был активирован.  Если вы регистрировались на сайте самостоятельно, вы должны были получить инструкции по активации профиля по электронной почте.';
-    ?></p>
-
-    <form method='post' action='/base.rpc?action=login&destination=<?php print urlencode($_SERVER['REQUEST_URI']) ?>' class='tabbed'>
-      <table align='center' border='0' cellspacing='0' cellpadding='4'>
-        <tr>
-          <td class='label'>
-            <label for='ctluser'>Имя:</label>
-          </td>
-          <td>
-            <input type='text' id='ctluser' name='login' />
-          </td class='control'>
-        </tr>
-        <tr>
-          <td class='label'>
-            <label for='ctlpass'>Пароль:</label>
-          </td>
-          <td class='control'>
-            <input type='password' id='ctlpass' name='password' />
-          </td>
-        </tr>
-        <tr>
-          <td>&nbsp;</td>
-          <td class='control'>
-            <input type='submit' id='ctlsubmit' value='Войти' />
-          </td>
-        </tr>
-      </table>
-    </form>
+    <div class="halfheight">&nbsp;</div>
+    <div id='form-user-login-form-wrapper'>
+      <h1 id="logo"><a><span>Molinos.CMS</span></a></h1>
+      <form method='post' action='<?php print $laction; ?>' id='user-login-form' <?php if (empty($_GET['login'])) print "class='active' " ?>enctype='multipart/form-data'>
+        <fieldset>
+          <legend>Авторизация</legend>
+          <div class='control control-TextLineControl-wrapper'>
+            <label for='unnamed-ctl-2' class='required'>Логин:</label>
+            <input type='text' id='unnamed-ctl-2' class='form-text' name='login' value='' />
+          </div>
+          <div class='control control-PasswordControl-wrapper'>
+            <label for='unnamed-ctl-3' class='required'>Пароль:</label>
+            <input type='password' id='unnamed-ctl-3' class='form-text' name='password' value='' />
+            <a class="restorepass_sh" href="<?php print $lforgot; ?>" onclick="javascript:return show_restore();">забыли пароль?</a>
+          </div>
+          <div class='control control-SubmitControl-wrapper'>
+            <input type='submit' class='form-submit' value='Войти' />
+          </div>
+        </fieldset>
+      </form>
+      <form method='post' action='/base.rpc?action=restore' id='profile-remind-form' <?php if (!empty($_GET['login'])) print "class='active' " ?>enctype='multipart/form-data'>
+        <fieldset>
+          <legend>Напоминание пароля</legend>
+          <div class='intro'>Введите почтовый адрес, который вы использовали при регистрации.  Инструкция по восстановлению пароля будет отправлена на этот адрес.</div>
+          <div class='control control-TextLineControl-wrapper'>
+            <!--
+            <div class='label'>
+              <label for='unnamed-ctl-1' class='required'>Логин или почтовый адрес:</label>
+            </div>
+            -->
+            <input type='text' id='unnamed-ctl-1' class='form-text' name='identifier' value='' />
+          </div>
+          <div class='control control-SubmitControl-wrapper'>
+            <input type='submit' class='form-submit' value='Напомнить' /> <a class="restorepass_sh" href="<?php print $lreturn; ?>" onclick="javascript:return show_login();">вернуться к авторизации</a>
+          </div>
+        </fieldset>
+      </form>
+    </div>
   </body>
 </html>
