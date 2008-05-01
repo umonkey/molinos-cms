884d0490b03263f9ac17e55475d0175282d136fa
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-05-01 14:36:30 +0000

    Обработка исключений в bebop_autoload().

--- lib/bebop_autoload.php
+++ lib/bebop_autoload.php
@@ -1,31 +1,37 @@
 <?php
+// vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
 
 function bebop_autoload($class_name)
 {
-    static $map = null;
+  static $map = null;
 
-    if (null === $map)
+  if (null === $map) {
+    try {
       $map = mcms::getClassMap();
-
-    $k = strtolower($class_name);
-
-    if (array_key_exists($k, $map)) {
-      if (!is_readable($map[$k]))
-        mcms::fatal("{$class_name} is in a file which is read-protected: {$map[$k]}");
-
-      include($map[$k]);
-
-      $isif = (substr($class_name, 0, 1) === 'i');
-
-      if ($isif and !in_array($class_name, get_declared_interfaces()))
-        mcms::fatal("There is no {$class_name} interface in {$map[$k]}", array('declared_interfaces' => get_declared_interfaces()));
-      elseif (!$isif and !in_array($class_name, get_declared_classes()))
-        mcms::fatal("There is no {$class_name} class in {$map[$k]}", array('declared_classes' => get_declared_classes()));
+    } catch (Exception $e) {
+      mcms::fatal('Exception during bebop_autoload(): '. $e->getMessage());
     }
+  }
 
-    else {
-      mcms::fatal("There was an attempt to access an undefined class {$class_name}.");
-    }
+  $k = strtolower($class_name);
+
+  if (array_key_exists($k, $map)) {
+    if (!is_readable($map[$k]))
+      mcms::fatal("{$class_name} is in a file which is read-protected: {$map[$k]}");
+
+    include($map[$k]);
+
+    $isif = (substr($class_name, 0, 1) === 'i');
+
+    if ($isif and !in_array($class_name, get_declared_interfaces()))
+      mcms::fatal("There is no {$class_name} interface in {$map[$k]}", array('declared_interfaces' => get_declared_interfaces()));
+    elseif (!$isif and !in_array($class_name, get_declared_classes()))
+      mcms::fatal("There is no {$class_name} class in {$map[$k]}", array('declared_classes' => get_declared_classes()));
+  }
+
+  else {
+    mcms::fatal("There was an attempt to access an undefined class {$class_name}.");
+  }
 }
 
 spl_autoload_register('bebop_autoload');
