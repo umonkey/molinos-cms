22cb8ddd5112e141a774dda09e9887ec8cde887c
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-06-04 14:24:45 +0000

    Все тесты в одном.

---
+++ lib/tests/AllTests.php
@@ -0,0 +1,36 @@
+#!/usr/local/bin/php -f
+<?php
+// Based on Alexei Zakhlestine's MySQL Query Builder,
+// http://code.google.com/p/mysql-query-builder/source/browse/trunk/tests/AllTests.php
+
+if (!defined('PHPUnit_MAIN_METHOD'))
+  define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require 'ConstTest.php';
+require 'UrlTest.php';
+require 'HtmlTest.php';
+
+class AllTests
+{
+  public static function main()
+  {
+    PHPUnit_TextUI_TestRunner::run(self::suite());
+  }
+
+  public static function suite()
+  {
+    $suite = new PHPUnit_Framework_TestSuite('PHPUnit Framework');
+
+    $suite->addTestSuite('ConstTest');
+    $suite->addTestSuite('UrlTest');
+    $suite->addTestSuite('HtmlTest');
+
+    return $suite;
+  }
+}
+
+if (PHPUnit_MAIN_METHOD == 'AllTests::main')
+  AllTests::main();
--- lib/tests/test.sh
+++
@@ -1,3 +0,0 @@
-phpunit ConstTest ConstTest.php
-phpunit UrlTest UrlTest.php
-phpunit HtmlTest HtmlTest.php
