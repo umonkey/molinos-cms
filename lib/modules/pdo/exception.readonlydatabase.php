e9fa39bb685570dba6866b77b5da7a05ed87d322
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-06-05 09:32:39 +0000

    R/O DB теперь выдаётся как Runtime, а не Forbidden.

--- lib/modules/pdo/exception.readonlydatabase.php
+++ lib/modules/pdo/exception.readonlydatabase.php
@@ -1,10 +1,10 @@
 <?php
 // vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
 
-class ReadOnlyDatabaseException extends ForbiddenException
+class ReadOnlyDatabaseException extends RuntimeException
 {
   public function __construct()
   {
-    parent::__construct(t('Вы попытались изменить содержимое базы данных, доступной только для чтения.'));
+    parent::__construct(t('База данных закрыта от записи, внесение изменений невозможно.'));
   }
 };
