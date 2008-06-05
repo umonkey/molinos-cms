c90dc9ad51ca805505149cd6a1a73f1775db3aae
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-06-05 10:14:02 +0000

    Несколько дефолтных полей для типов документов.

--- lib/modules/base/node.type.php
+++ lib/modules/base/node.type.php
@@ -8,6 +8,34 @@ class TypeNode extends Node implements iContentType, iScheduler, iModuleConfig
   public function __construct(array $data)
   {
     parent::__construct($data);
+
+    if (empty($this->data['id']) and empty($this->data['fields'])) {
+      $this->data['fields'] = array(
+        'name' => array(
+          'label' => 'Заголовок',
+          'type' => 'TextLineControl',
+          'required' => true,
+          'indexed' => true,
+          ),
+        'teaser' => array(
+          'label' => 'Вступление',
+          'type' => 'TextAreaControl',
+          'required' => false,
+          'description' => 'Краткое содержание, 1-2 предложения.  Выводится в списках документов, RSS итд.',
+          ),
+        'text' => array(
+          'label' => 'Текст',
+          'type' => 'TextHTMLControl',
+          'required' => true,
+          ),
+        'created' => array(
+          'label' => 'Дата добавления',
+          'type' => 'DateTimeControl',
+          'required' => false,
+          'indexed' => true,
+          ),
+        );
+    }
   }
 
   // Инсталляция типов документов.
