08f126d5fe4bfd38b1901e175be10741c3ac64b4
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-07-03 17:17:04 +0000

    Issue 315: манипуляция изображениями.

--- lib/modules/imgtoolkit/class.imageedit.php
+++ lib/modules/imgtoolkit/class.imageedit.php
@@ -1,66 +1,80 @@
 <?php
 
-require(realpath(dirname(__FILE__) . '/lib/bootstrap.php'));
-
-$att = new ImageEditor($_GET);
-$att->sendFile();
-
-class ImageEditor
+class ImageEdit implements iRemoteCall
 {
-  var $folder;
-  var $filename;
-  var $source;
-  var $output;
-  var $nw;
-  var $nh;
-  var $options = array(
-      'scale' => 100, 
-      'mirrorV' => 0, 
-      'mirrorH' => 0, 
-      'angle' => 0, 
-      'offsetX' => 0, 
+  private $folder;
+  private $filename;
+  private $source;
+  private $output;
+  private $nw;
+  private $nh;
+
+  public static function hookRemoteCall(RequestContext $ctx)
+  {
+    $img = new ImageEdit($ctx);
+    $img->sendFile();
+    exit();
+  }
+
+  // Набор параметров, которые используются для редактирования изображения,
+  // подробности - в документации
+  private $options = array(
+      'fid' => null,
+      'scale' => 100,
+      'mirrorV' => false,
+      'mirrorH' => false,
+      'angle' => 0,
+      'offsetX' => 0,
       'offsetY' => 0,
-      'cropX' => 0,
-      'cropY' => 0,
-      'cropW' => 0,
-      'cropH' => 0,
+      'cropLeft' => 0,
+      'cropTop' => 0,
+      'cropRight' => 0,
+      'cropBottom' => 0,
       'merge' => null,
-      );
+      'noshow' => false,
+  );
 
   private $node = null;
   private $mergeNode = null;
 
-  public function __construct($get)
+  public function __construct(RequestContext $ctx)
   {
-    $args = explode(',', $get['q']);
-
-    $node = Tagger::getInstance()->getObject($args[0]);
-    if (empty($node))
-      $this->sendError(404, 'attachment not found.');
-
-    $this->node = $node;
-    $this->filename = $node['filename'];
-    $this->sourceDir = realpath(dirname(dirname(__FILE__)) .'/attachments');
-    $this->source = realpath(dirname(dirname(__FILE__)) .'/attachments/'. $node['filepath']);
-
     // Если кроме id аттачмента ничего не задали, ругаемся
-    if (2 > sizeof($args))
-        $this->sendError(500, 'usage: '.$get['folder'].'/filename[,scale[,mirrorV[,mirrorH[,offsetX[,offsetY[,cropLeft[,cropTop[,cropRight[,cropBottom]]]]]]]]]');
+    if (null == $ctx->get('fid', null) or null == $ctx->get('scale', null))
+    	$this->sendError(500, 'usage: imgtoolkit.rpc&fid=attachment[&operation=parameter...]');
 
-    $this->folder = realpath(dirname(__FILE__) .'/attachment');
-    $this->output = $this->folder . '/' . $get['q'];
-
-    // Сохраняем параметры для обработки изображения
-    array_shift($args);
-    $i = 0;
     foreach ($this->options as $k => $v) {
-      if (!empty($args[$i]))
-        $this->options[$k] = floatval($args[$i]);
-      $i++;
+      if ($p = $ctx->get($k)) {
+      	if (in_array($k, array('noshow', 'merge', 'fid', 'mirrorH', 'mirrorV')))
+	        $this->options[$k] = $p;
+        else
+	        $this->options[$k] = floatval($p);
+      }
+      // Формируем имя файла-ссылки для кэша
+      $outfile .= "{$this->options[$k]},";
     }
 
-    if (null != $this->options['merge'])
-      $this->mergeNode = Node::load($this->options['merge']);
+    $outfile = trim($outfile, ',');
+    $node = Node::load(array('class' => 'file', 'id' => $this->options['fid']));
+
+    if (empty($node))
+      self::sendError(404, 'attachment not found.');
+
+    if (null === ($storage = mcms::config('filestorage')))
+      $storage = 'storage';
+
+    $this->node = $node;
+    $this->filename = $node->filename;
+    $this->folder = $storage;
+    $this->sourceDir = $storage;
+    $this->source = $storage . '/' . $node->filepath;
+    $this->output = $this->folder . '/' . $outfile;
+
+    if (null != $this->options['merge']) {
+	    $this->mergeNode = Node::load(array('class' => 'file', 'id' => $this->options['merge']));
+	    if (empty($this->mergeNode))
+	    	self::sendError(404, 'merge attachment not found.');
+    }
   }
 
   private function sendError($code, $more = null)
@@ -95,7 +109,7 @@ class ImageEditor
 
     // Пытаемся открыть файл на чтение.  Сработает только если файл является картинкой.
     $img = ImageMagick::getInstance();
-    $rc = $img->open($this->source, $this->node['filetype']);
+    $rc = $img->open($this->source, $this->node->filetype);
     
     // Если не удалось открыть, значит файл не является картинкой.  Отдаём его в режиме скачивания.
     if ($rc === false)
@@ -113,21 +127,22 @@ class ImageEditor
 
       if (is_link($this->output) and !unlink($this->output))
         $this->sendError(500, 'could not remove the file from cache. Overwriting that file would cause the original data to be lost.');
-
-      // Если масштабирование не запрошено -- создаём симлинк на картинку,
-      // чтобы больше к её обработке не возвращаться (загоняем в кэш).
-//      elseif (!symlink($this->source, $this->output))
-//          $this->sendError(500, "could not create a caching symlink for this attachment.");
     }
 
-    // Открываем файл на чтение.
-    $f = fopen($this->output, 'r')
-      or $this->sendError(500, "could not read from cache.");
+    // Бывают ситуации, когда картинку не нужно отдавать в браузер,
+    // чтобы не заставлять клиента грузить ненужные килобайты, а то и мегабайты.
+    if (false == $this->options['noshow']) {
+    	// Открываем файл на чтение.
+    	$f = fopen($this->output, 'r')
+    	or $this->sendError(500, "could not read from cache.");
 
-    // Отправляем файл клиенту.
-    header('Content-Type: '.$img->getType());
-    header('Content-Length: '.filesize($this->output));
-    fpassthru($f);
+    	// Отправляем файл клиенту.
+    	header('Content-Type: '.$img->getType());
+    	header('Content-Length: '.filesize($this->output));
+    	fpassthru($f);
+    } else {
+    	exit();
+    }
   }
 
   private function processImage($img)
@@ -143,11 +158,11 @@ class ImageEditor
         $this->sendError(500, "could not resize the image");
 
       // Отзеркаливание по вертикали
-      if (1 == $this->options['mirrorV'])
+      if (true == $this->options['mirrorV'])
         $img->mirror('v');
 
       // Отзеркаливание по горизонтали
-      if (1 == $this->options['mirrorH'])
+      if (true == $this->options['mirrorH'])
         $img->mirror('h');
 
       // Поворот на заданный угол
@@ -165,10 +180,10 @@ class ImageEditor
 
       // Кадрирование по заданным габаритам
       $size = $img->getImageSize();
-      $dX = $this->options['cropX'];
-      $dY = $this->options['cropY'];
-      $dW = $this->options['cropW'];
-      $dH = $this->options['cropH'];
+      $dX = $this->options['cropLeft'];
+      $dY = $this->options['cropTop'];
+      $dW = $this->options['cropRight'];
+      $dH = $this->options['cropBottom'];
 
       $nW = $size[0] - $dW - $dX;
       $nH = $size[1] - $dH - $dY;
@@ -199,13 +214,13 @@ class ImageEditor
     ini_set('zlib.output_compression', 0);
 
     $headers = array();
-    $headers[] = "Content-Type: ". $this->node['filetype'];
-    $headers[] = "Content-Length: ". $this->node['filesize'];
+    $headers[] = "Content-Type: ". $this->node->filetype;
+    $headers[] = "Content-Length: ". $this->node->filesize;
 
-    $download = (strstr($this->node['filetype'], 'shockwave') === false);
+    $download = (strstr($this->node->filetype, 'shockwave') === false);
 
     if ($download)
-      $headers[] = "Content-Disposition: attachment; filename=\"{$this->node['filename']}\"";
+      $headers[] = "Content-Disposition: attachment; filename=\"{$this->node->filename}\"";
 
     foreach ($headers as $item)
       header($item);
@@ -214,4 +229,4 @@ class ImageEditor
       or $this->sendError(403, "could not read the file");
     fpassthru($f);
   }
-};
+}
