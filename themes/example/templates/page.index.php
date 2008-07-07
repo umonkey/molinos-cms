77cf0a1f2327ce431ccf2cee7c067403635df5a1
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-07-07 12:28:10 +0000

    Очистка шаблонов примера.

--- themes/example/templates/page.index.php
+++ themes/example/templates/page.index.php
@@ -5,35 +5,33 @@ mcms::extras('themes/example/styles/lib/refpoint.typography-16.css');
 mcms::extras('themes/example/styles/lib/refpoint.logo.css');
 mcms::extras('themes/example/styles/page.index.css');
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
-	<head>
-		<title>Главная страница - </title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta name="robots" content="index,follow" />
-	  <base href='<?php print $base; ?>' />
-		<link rel="shortcut icon" href="themes/all/img/favicon.ico" type="image/x-icon" />
+  <head>
+    <title>Главная страница — Molinos CMS</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="index,follow" />
+    <base href='<?php print $base; ?>' />
+    <link rel="shortcut icon" href="themes/all/img/favicon.ico"
+      type="image/x-icon" />
     <?php print mcms::extras(); ?>
-		<!--
-		<script type="text/javascript" src="themes/example/scripts/lib/jquery.js"></script>
-		<script type="text/javascript" src="themes/example/scripts/pages.ordinary.js"></script>
-		-->
-	</head>
-	<body>
-		
-		<div id="navigation">
-			<?php if (!empty($widgets['sections'])) print $widgets['sections']; ?>
-		</div>
-		
-		<div id="content">
-			<h1 id="logo"><a href="."><span>Molinos.CMS</span></a></h1>
-		
-			<?php if (!empty($widgets['doclist'])) print $widgets['doclist']; ?>
-		
-		</div>
-		
-	</body>
+  </head>
+  <body>
+    
+    <div id="navigation">
+      <?php if (!empty($widgets['sections'])) print $widgets['sections']; ?>
+    </div>
+    
+    <div id="content">
+      <h1 id="logo"><a href="."><span>Molinos CMS</span></a></h1>
+    
+      <?php if (!empty($widgets['doclist'])) print $widgets['doclist']; ?>
+    
+    </div>
+    
+  </body>
   <!-- request time: $execution_time ms. -->
 </html>
--- themes/example/templates/page.node.php
+++ themes/example/templates/page.node.php
@@ -1,36 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+
+mcms::extras('themes/example/styles/lib/refpoint.reset.css');
+mcms::extras('themes/example/styles/lib/refpoint.typography-16.css');
+mcms::extras('themes/example/styles/lib/refpoint.logo.css');
+mcms::extras('themes/example/styles/page.index.css');
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
-	<head>
-		<title>Главная страница - </title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta name="robots" content="index,follow" />
-		 <base href='<?php print $base; ?>' />
-		<!--
-		<link rel="shortcut icon" href="themes/example/images/favicon.ico" type="image/x-icon" />
-		-->
-		<link rel="stylesheet" href="themes/example/styles/lib/refpoint.reset.css" type="text/css" />
-		<link rel="stylesheet" href="themes/example/styles/lib/refpoint.typography-16.css" type="text/css" />
-		<link rel="stylesheet" href="themes/example/styles/lib/refpoint.logo.css" type="text/css" />
-		<link rel="stylesheet" href="themes/example/styles/page.index.css" type="text/css" />
-		<!--
-		<script type="text/javascript" src="themes/example/scripts/lib/jquery.js"></script>
-		<script type="text/javascript" src="themes/example/scripts/pages.ordinary.js"></script>
-		-->
-	</head>
-	<body>
-		
-		<div id="navigation">
-			<?= $widgets['sections'] ?>
-		</div>
-		
-		<div id="content">
-			<h1 id="logo"><a href=""><span>Molinos.CMS</span></a></h1>
-			
-			<?= $widgets['doc'] ?>
-			
-		</div>
-		
-	</body>
+  <head>
+    <title>Просмотр документа — Molinos CMS</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="index,follow" />
+    <base href='<?php print $base; ?>' />
+		<link rel="shortcut icon" href="themes/all/img/favicon.ico"
+      type="image/x-icon" />
+    <?php print mcms::extras(); ?>
+  </head>
+  <body>
+    
+    <div id="navigation">
+      <?php print $widgets['sections']; ?>
+    </div>
+    
+    <div id="content">
+      <h1 id="logo"><a href="."><span>Molinos CMS</span></a></h1>
+      
+      <?php print $widgets['doc']; ?>
+    </div>
+  </body>
 </html>
