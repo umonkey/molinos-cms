5ef72a212de823212595decac1c96acdaa3bce4a
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-07-21 18:55:03 +0000

    Тест на создание системных таблиц.

--- lib/tests/PdoTest.php
+++ lib/tests/PdoTest.php
@@ -150,4 +150,23 @@ class PdoTest extends PHPUnit_Framework_TestCase
     $t = new TableInfo('node');
     $this->assertTrue($t->exists());
   }
+
+  public function testInternalTables()
+  {
+    $tables = array(
+      'node',
+      'node__access',
+      'node__cache',
+      'node__log',
+      'node__rel',
+      'node__rev',
+      'node__session',
+      );
+
+    foreach ($tables as $table) {
+      $c = mcms::db()->fetch("SELECT COUNT(*) FROM `{$table}`");
+      $t = new TableInfo($table);
+      $this->assertTrue($t->exists());
+    }
+  }
 }
