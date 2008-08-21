40d3a0c37f8c91fd07196a8fbc5851c3444797cc
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-08-21 10:24:59 +0000

    Удалены неиспользуемые интерфейсы iFormObject и iNodePatchHook.

--- lib/modules/base/interface.formobject.php
+++
@@ -1,9 +0,0 @@
-<?php
-// vim: expandtab tabstop=2 shiftwidth=2 softtabstop=2:
-
-interface iFormObject
-{
-  public function formGet();
-  public function formGetData();
-  public function formProcess(array $data);
-};
--- lib/modules/base/interface.nodepatchhook.php
+++
@@ -1,8 +0,0 @@
-<?php
-// vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
-
-interface iNodePatchHook
-{
-  public static function patchNode(Node $node);
-  public static function patchNodeList(array &$nodes);
-};
