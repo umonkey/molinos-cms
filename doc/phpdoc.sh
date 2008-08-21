6bbaf3b6868b51b7af0cba24717354125d8ceef0
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-08-21 10:20:47 +0000

    Скрипт для построения документации.

---
+++ doc/phpdoc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd $(dirname $0)
+if [ ! -d phpdoc ]; then
+    mkdir phpdoc
+else
+    rm -rf phpdoc/*
+fi
+phpdoc -dn 'mod_base' -t phpdoc -d ../lib/modules/base --ignore '*.phtml,*.png,*.cs,*.js,tests.php' -o HTML:default:default --title "Molinos CMS Documentation"
