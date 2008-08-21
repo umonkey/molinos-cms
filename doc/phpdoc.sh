26e55d22024da077765feffcc60a424cc3a02a38
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-08-21 10:23:32 +0000

    Закачка документации на сайт после производства.

--- doc/phpdoc.sh
+++ doc/phpdoc.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+DIR=tmpdoc
 cd $(dirname $0)
 if [ ! -d phpdoc ]; then
-    mkdir phpdoc
+    mkdir "$DIR"
 else
-    rm -rf phpdoc/*
+    rm -rf "$DIR"/*
 fi
-phpdoc -dn 'mod_base' -t phpdoc -d ../lib/modules/base --ignore '*.phtml,*.png,*.cs,*.js,tests.php,*.test.php' -o HTML:default:default --title "Molinos CMS Documentation"
+phpdoc -dn 'mod_base' -t "$DIR" -d ../lib/modules/base --ignore '*.phtml,*.png,*.cs,*.js,tests.php,*.test.php' -o HTML:default:default --title "Molinos CMS Documentation"
