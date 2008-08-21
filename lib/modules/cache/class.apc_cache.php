834a2d83eb67d0ec532e99fa6424400e2550a13c
justin.forest <justin.forest@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-08-21 10:28:36 +0000

    Документация по APC_Cache.

--- lib/modules/cache/class.apc_cache.php
+++ lib/modules/cache/class.apc_cache.php
@@ -1,6 +1,21 @@
 <?php
-// vim: expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+/**
+ * Оперативное кэширование данных в APC.
+ *
+ * @package mod_cache
+ * @author Justin Forest <justin.forest@gmail.com>
+ * @copyright 2006-2008 Molinos.RU
+ * @license http://www.gnu.org/copyleft/gpl.html GPL
+ */
 
+/**
+ * Оперативное кэширование данных в APC.
+ *
+ * @package mod_cache
+ * @author Justin Forest <justin.forest@gmail.com>
+ * @copyright 2006-2008 Molinos.RU
+ * @license http://www.gnu.org/copyleft/gpl.html GPL
+ */
 class APC_Cache implements iBebopCacheEngine
 {
     private static $instance = null;
@@ -15,6 +30,11 @@ class APC_Cache implements iBebopCacheEngine
         $this->prefix = 'bbp_'.hash('crc32', __FILE__).'_'; //making prefix, which is unique to this project
     }
 
+    /**
+     * Получение объекта для работы с кэшем.
+     *
+     * @return APC_Cache интерфейс к кэшу.
+     */
     static public function getInstance()
     {
         if (self::$instance === null) {
@@ -24,6 +44,11 @@ class APC_Cache implements iBebopCacheEngine
         return self::$instance;
     }
 
+    /**
+     * Проверка наличия APC.
+     *
+     * @return bool true, если APC есть.
+     */
     static public function isAvailable()
     {
         return function_exists('apc_store');
@@ -49,12 +74,23 @@ class APC_Cache implements iBebopCacheEngine
         apc_delete($this->prefix.$varname);
     }
 
+    /**
+     * Определение размера кэша.
+     *
+     * @return integer количество объектов в кэше.
+     */
     public function count()
     {
         $stats = apc_cache_info('user');
         return count($stats['cache_list']);
     }
 
+    /**
+     * Очистка кэша.
+     *
+     * @param bool $now true если кэш нужно очистить немедленно, false если
+     * можно потом.
+     */
     public function flush($now = false)
     {
         if ($now and $this->flush) {
@@ -65,6 +101,15 @@ class APC_Cache implements iBebopCacheEngine
         }
     }
 
+    /**
+     * Получение префикса.
+     *
+     * Префикс используется для обеспечения уникальности в именах кэшируемых
+     * объектов (наложение возникает когда несколько сайтов используют общий
+     * кэш).
+     *
+     * @return string префикс для переменных.
+     */
     public function getPrefix()
     {
         return $this->prefix;
