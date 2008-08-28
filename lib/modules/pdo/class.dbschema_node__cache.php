29c7f7070afd6d4d17aeb379c0f51e29454d868b
Justin Forest <justin.forest@gmail.com>
2008-08-28 19:43:28 +0400

    Пропавший ключ возвращён в node__cache.

--- lib/modules/pdo/class.dbschema_node__cache.php
+++ lib/modules/pdo/class.dbschema_node__cache.php
@@ -5,17 +5,13 @@ class DBSchema_node__cache extends TableManager
   public function __construct()
   {
     $this->columns['cid'] =  array(
-        'type' => 'char(32)',
-        'required' => true,
-        );
-
-    $this->columns['lang'] =  array(
-        'type' => 'char(2)',
-        'required' => true,
-        );
+      'type' => 'char(32)',
+      'required' => true,
+      'key' => 'pri',
+      );
 
     $this->columns['data'] =  array(
-        'type' => 'mediumblob',
-        );
+      'type' => 'mediumblob',
+      );
   }
 }
