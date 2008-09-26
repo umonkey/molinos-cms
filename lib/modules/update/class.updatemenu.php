8560efbe0a6c9962528721c666d981a1e90d07bb
Justin Forest <justin.forest@gmail.com>
2008-09-26 21:43:26 +0400

    mod_update, themes/admin: проверка обновлений по крону.

---
+++ lib/modules/update/class.updatemenu.php
@@ -0,0 +1,23 @@
+<?php
+
+class UpdateMenu implements iAdminMenu
+{
+  public static function getMenuIcons()
+  {
+    $icons = array();
+
+    if (file_exists($tmp = mcms::config('tmpdir') .'/updates.txt')) {
+      list($version, $filename) = explode(',', file_get_contents($tmp));
+
+      $icons[] = array(
+        'group' => 'status',
+        'message' => t('<span class="updates">Есть обновление CMS: %version, <a href="@url">установить</a>?</span>', array(
+          '%version' => $version,
+          '@url' => '?q=admin&cgroup=none&module=update'
+          )),
+        );
+    }
+
+    return $icons;
+  }
+}
