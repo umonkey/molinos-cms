c6beecc7cd1c578849129a2b039f63c26e1a47ef
Justin Forest <justin.forest@gmail.com>
2008-10-08 13:42:49 +0400

    modules/admin: поправлено добавление первой страницы в домен.

--- lib/modules/admin/class.admintreehandler.php
+++ lib/modules/admin/class.admintreehandler.php
@@ -61,8 +61,9 @@ class AdminTreeHandler
       $data = self::getNodeTree();
 
       if (empty($data))
-        mcms::redirect("?q=admin&mode=create&type={$this->type}"
+        mcms::redirect("?q=admin&mode=create"
           ."&parent=". $this->ctx->get('subid')
+          ."&type={$this->type}"
           ."&destination=CURRENT");
 
       return $data;
