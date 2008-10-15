b36ace1eef5fb9e28579ebcdb326ba8ee9021e61
Justin Forest <justin.forest@gmail.com>
2008-10-15 19:02:27 +0400

    Поправлен новый загрузчик.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -4,18 +4,23 @@
 define('MCMS_VERSION', '8.05.6194');
 
 // Полный путь к папке, в которую установлена CMS.
-define('MCMS_ROOT', dirname(realpath(__FILE__)));
+define('MCMS_ROOT', realpath(dirname(realpath(__FILE__)) .'/..'));
 
 // Работа в нормальном режиме.
-if (file_exists($bootstrap = dirname(__FILE__) .'/lib/'. MCMS_VERSION .'/loader.php'))
-  require_once $bootstrap;
+if (file_exists($bootstrap = dirname(__FILE__) .'/'. MCMS_VERSION .'/loader.php'))
+  ;
 
 // Работа прямо из git.
-elseif (file_exists($bootstrap = dirname(__FILE__) .'/lib/loader.php'))
-  require_once $bootstrap;
+elseif (file_exists($bootstrap = dirname(__FILE__) .'/loader.php'))
+  ;
 
 else {
   header('HTTP 500 Internal Server Error');
   header('Content-Type: text/plain; charset=utf-8');
   die('Molinos CMS loader not found in lib/'. MCMS_VERSION ."\n");
 }
+
+// Путь к системным файлам.
+define('MCMS_LIB', dirname($bootstrap));
+
+require $bootstrap;
