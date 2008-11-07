34c066422f7e342252875cfba51342bf0ff50523
Justin Forest <justin.forest@gmail.com>
2008-11-07 17:19:22 +0300

    Минимальная версия увеличена до 5.2.0.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+if (version_compare(PHP_VERSION, "5.2.0", "<"))
+  die('Для работы Molinos CMS требуется PHP версии 5.2.0, или более новый.');
+
 require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
 mcms::run();
