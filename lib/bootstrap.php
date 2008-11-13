05894c552d0e6a707f694142e8b0ea9a83c363b8
Justin Forest <justin.forest@gmail.com>
2008-11-13 18:20:39 +0300

    Поправлено сканирование модулей.
    Не загружаются классы из неиспользуемых модулей.  Отключенные
    модули сохраняются в карте, но без классов.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -13,7 +13,7 @@ class Loader
 {
   public static function rebuild($local = false)
   {
-    $map = self::scan($local);
+    $map = self::scan($local, preg_split('/,\s*/', mcms::config('runtime_modules')));
 
     $path = MCMS_LIB . DIRECTORY_SEPARATOR . ($local
       ? 'classpath.local.inc'
@@ -25,7 +25,7 @@ class Loader
       or mcms::fatal('Could not update classpath.inc');
   }
 
-  private static function scan($local = false)
+  private static function scan($local = false, $enabled_modules = null)
   {
     $result = array(
       'modules' => array(),
@@ -51,7 +51,7 @@ class Loader
         'classes' => array(),
         'interfaces' => array(),
         'implementors' => array(),
-        'enabled' => $modok = true, // in_array($modname, $enabled),
+        'enabled' => $modok = ($enabled_modules === null or (is_array($enabled_modules) and in_array($modname, $enabled_modules))),
         );
 
       if (is_array($ini = parse_ini_file($modinfo, true))) {
@@ -60,85 +60,89 @@ class Loader
           if (array_key_exists($k, $ini)) {
             $result['modules'][$modname][$k] = $ini[$k];
 
-            if ('group' == $k and 'core' == strtolower($ini[$k]))
+            if ('group' == $k and !strcasecmp('core', $ini[$k]))
               $modok = $result['modules'][$modname]['enabled'] = true;
           }
         }
       }
 
-      // Составляем список доступных классов.
-      foreach (glob($path . DIRECTORY_SEPARATOR . '*.php') as $classpath) {
-        $parts = explode('.', basename($classpath), 3);
+      if ($modok) {
+        // Составляем список доступных классов.
+        foreach (glob($path . DIRECTORY_SEPARATOR . '*.php') as $classpath) {
+          $parts = explode('.', basename($classpath), 3);
 
-        if (count($parts) != 3 or $parts[2] != 'php')
-          continue;
+          if (count($parts) != 3 or $parts[2] != 'php')
+            continue;
 
-        $classname = null;
+          $classname = null;
 
-        switch ($type = $parts[0]) {
-        case 'class':
-          $classname = $parts[1];
-          break;
-        case 'control':
-        case 'node':
-        case 'widget':
-        case 'exception':
-          $classname = $parts[1] . $type;
-          break;
-        case 'interface':
-          $classname = 'i'. $parts[1];
-          break;
-        }
-
-        if (null !== $classname and is_readable($classpath)) {
-          // Добавляем в список только первый найденный класс.
-          if ($modok and !array_key_exists($classname, $result['classes'])) {
-            $result['classes'][$classname] = $classpath;
-            $result['rclasses'][$classname] = $modname;
+          switch ($type = $parts[0]) {
+          case 'class':
+            $classname = $parts[1];
+            break;
+          case 'control':
+          case 'node':
+          case 'widget':
+          case 'exception':
+            $classname = $parts[1] . $type;
+            break;
+          case 'interface':
+            $classname = 'i'. $parts[1];
+            break;
           }
 
-          // Строим список интерфейсов.
-          if ($type !== 'interface') {
-            if (preg_match('@^\s*(abstract\s+){0,1}class\s+([^\s]+)(\s+extends\s+([^\s]+))*(\s+implements\s+([^\n\r]+))*@im', file_get_contents($classpath), $m)) {
-              $classname = $m[2];
+          if (null !== $classname and is_readable($classpath)) {
+            // Добавляем в список только первый найденный класс.
+            if ($modok and !array_key_exists($classname, $result['classes'])) {
+              $result['classes'][$classname] = $classpath;
+              $result['rclasses'][$classname] = $modname;
+            }
 
-              $result['modules'][$modname]['classes'][] = $classname;
+            // Строим список интерфейсов.
+            if ($type !== 'interface') {
+              if (preg_match('@^\s*(abstract\s+){0,1}class\s+([^\s]+)(\s+extends\s+([^\s]+))*(\s+implements\s+([^\n\r]+))*@im', file_get_contents($classpath), $m)) {
+                $classname = $m[2];
 
-              if (!empty($m[6]))
-                $interfaces = preg_split('/[,\s]+/', preg_replace('#/\*.*\*/#', '', $m[6]), -1, PREG_SPLIT_NO_EMPTY);
-              else
-                $interfaces = array();
+                $result['modules'][$modname]['classes'][] = $classname;
 
-              if (!empty($m[4])) {
-                switch (strtolower($m[4])) {
-                case 'control':
-                  $interfaces[] = 'iFormControl';
-                  break;
-                case 'widget':
-                  $interfaces[] = 'iWidget';
-                  break;
-                case 'node':
-                case 'nodebase':
-                  $interfaces[] = 'iContentType';
-                  break;
+                if (!empty($m[6]))
+                  $interfaces = preg_split('/[,\s]+/', preg_replace('#/\*.*\*/#', '', $m[6]), -1, PREG_SPLIT_NO_EMPTY);
+                else
+                  $interfaces = array();
+
+                if (!empty($m[4])) {
+                  switch (strtolower($m[4])) {
+                  case 'control':
+                    $interfaces[] = 'iFormControl';
+                    break;
+                  case 'widget':
+                    $interfaces[] = 'iWidget';
+                    break;
+                  case 'node':
+                  case 'nodebase':
+                    $interfaces[] = 'iContentType';
+                    break;
+                  }
                 }
-              }
 
-              foreach ($interfaces as $i) {
-                if (!in_array($i, $result['modules'][$modname]['interfaces']))
-                  $result['modules'][$modname]['interfaces'][] = $i;
-                $result['modules'][$modname]['implementors'][$i][] = $classname;
+                foreach ($interfaces as $i) {
+                  if (!in_array($i, $result['modules'][$modname]['interfaces']))
+                    $result['modules'][$modname]['interfaces'][] = $i;
+                  $result['modules'][$modname]['implementors'][$i][] = $classname;
 
-                if ($modok)
-                  $result['interfaces'][strtolower($i)][] = strtolower($classname);
+                  if ($modok)
+                    $result['interfaces'][strtolower($i)][] = strtolower($classname);
+                }
               }
             }
           }
         }
       }
 
+      /*
       if (empty($result['modules'][$modname]['classes']))
         unset($result['modules'][$modname]);
+      */
     }
 
     ksort($result['classes']);
