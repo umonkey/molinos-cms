5facefdb3316a2b14632e6ffd127e22f18728679
Justin Forest <justin.forest@gmail.com>
2008-11-19 18:43:53 +0300

    Удалена ссылка на phpMiniAdmin.

--- themes/admin/templates/page.admin.php
+++ themes/admin/templates/page.admin.php
@@ -40,16 +40,6 @@ function render_username()
     ));
 }
 
-function get_dba_link()
-{
-  if ('MySQL' != mcms::db()->getDbType())
-    return;
-  
-  if (is_readable(MCMS_ROOT .'/phpminiadmin.php'))
-    return mcms::html('a', array('title' => 'БД',
-      'href' => 'phpminiadmin.php?showcfg=1'), 'БД');
-}
-
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -107,8 +97,7 @@ function get_dba_link()
         <div id="top_toolbar">
           <div class="right">
             <?php print render_username(); ?>
-            <a href="<?php print $base; ?>" title="Перейти на сайт"><img src="themes/admin/img/icon-home.png" alt="home" width="16" height="16" /></a>
-            <?php print get_dba_link(); ?>
+            <a href="?q=admin" title="Вернуться к началу"><img src="themes/admin/img/icon-home.png" alt="home" width="16" height="16" /></a>
             <a href="<?php print mcms_plain(l('?q=admin.rpc&action=reload&destination=CURRENT')); ?>&amp;reload=1&amp;flush=1" title="Очистить кэш"><img src="themes/admin/img/icon-reload.png" alt="reload" width="16" height="16" /></a>
             <a href="?q=base.rpc&amp;action=logout" id="lnk_exit" title="Выйти"><img src="themes/admin/img/icon-exit.png" alt="logout" width="16" height="16" /></a>
           </div>
