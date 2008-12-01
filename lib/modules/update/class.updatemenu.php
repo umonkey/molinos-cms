dc4ddce03a55bbda281f8af2b8fc237df8591bb5
Justin Forest <justin.forest@gmail.com>
2008-12-01 17:09:37 +0300

    Поправлена проверка обновлений.
    Больше не выводит сообщения о доступности версии если она
    младше текущей (возникало при использовании кандиидатов в релизы).

--- lib/modules/update/class.updatemenu.php
+++ lib/modules/update/class.updatemenu.php
@@ -20,10 +20,11 @@ class UpdateMenu implements iAdminMenu
     if (file_exists($tmp = mcms::config('tmpdir') . DIRECTORY_SEPARATOR . 'update.txt')) {
       list($version, $filename) = explode(',', file_get_contents($tmp));
 
-      return t('<span class="updates">Есть обновление CMS: %version, его можно <a href="@url">установить</a>.</span>', array(
-          '%version' => $version,
-          '@url' => '?q=admin&cgroup=none&module=update'
-          ));
+      if (version_compare($version, mcms::version()) == 1)
+        return t('<span class="updates">Есть обновление CMS: %version, его можно <a href="@url">установить</a>.</span>', array(
+            '%version' => $version,
+            '@url' => '?q=admin&cgroup=none&module=update'
+            ));
     }
   }
 }
