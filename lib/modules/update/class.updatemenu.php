77bf979aa2e2c4f887c5e1c93fdad0761145b884
Justin Forest <justin.forest@gmail.com>
2008-12-01 16:38:49 +0300

    Поправлено сообщение о наличии обновлений.

--- lib/modules/update/class.updatemenu.php
+++ lib/modules/update/class.updatemenu.php
@@ -20,7 +20,7 @@ class UpdateMenu implements iAdminMenu
     if (file_exists($tmp = mcms::config('tmpdir') . DIRECTORY_SEPARATOR . 'update.txt')) {
       list($version, $filename) = explode(',', file_get_contents($tmp));
 
-      return t('<span class="updates">Есть обновление CMS: %version, <a href="@url">установить</a>?</span>', array(
+      return t('<span class="updates">Есть обновление CMS: %version, его можно <a href="@url">установить</a>.</span>', array(
           '%version' => $version,
           '@url' => '?q=admin&cgroup=none&module=update'
           ));
