bc6163e8d05dfb8de34e5b5d3bd5ee79ad0a250d
Justin Forest <justin.forest@gmail.com>
2008-12-08 17:26:11 +0300

    {tagger_get_section} работает только с разделами.

--- lib/modules/smarty/plugins/function.tagger_get_section.php
+++ lib/modules/smarty/plugins/function.tagger_get_section.php
@@ -3,10 +3,15 @@
 
 function smarty_function_tagger_get_section($params, &$smarty)
 {
-  if (!empty($params['root']) and !empty($params['assign'])) {
-    $nid = is_array($params['root']) ? $params['root']['id'] : $params['root'];
-    $node = Node::load($nid);
+  if (empty($params['root']) or empty($params['assign']))
+    throw new SmartyException(t('{tagger_get_section} требует параметров root и assign.'));
 
-    $smarty->assign($params['assign'], $node->getChildren('nested'));
-  }
+  $filter = array(
+    'id' => Node::_id($params['root']),
+    'class' => 'tag',
+    );
+
+  $node = Node::load($filter);
+
+  $smarty->assign($params['assign'], $node->getChildren('nested'));
 }
