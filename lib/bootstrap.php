5e5a3e76f0c2d06d32b6311746978c48ce711ae5
Justin Forest <justin.forest@gmail.com>
2008-12-08 17:26:31 +0300

    8.05.6198RC1

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Текущая версия.
-define('MCMS_VERSION', '8.05.6198DEV');
+define('MCMS_VERSION', '8.05.6198RC1');
 
 // Начало обработки запроса, для замеров производительности.
 define('MCMS_START_TIME', microtime(true));
--- lib/modules/smarty/plugins/function.tagger_get_nodes.php
+++
@@ -1,7 +0,0 @@
-<?php
-// vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2
-
-function smarty_function_tagger_get_nodes($params, &$smarty)
-{
-  throw new SmartyException(t('{tagger_get_nodes} больше не поддерживается.'));
-}
