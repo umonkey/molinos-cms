741cad0a1bd523398290e7032c4f63a4081a851a
Justin Forest <justin.forest@gmail.com>
2008-12-10 14:33:06 +0300

    Скрипт для формирования общего modules.ini

---
+++ tools/rebuild-distinfo.php
@@ -0,0 +1,33 @@
+<?php
+
+require dirname(__FILE__) .'/../lib/bootstrap.php';
+
+$ini = array();
+
+foreach (glob(os::path('lib', 'modules', '*', 'module.ini')) as $file) {
+  $module = basename(dirname($file));
+  $ini[$module] = ini::read($file);
+
+  foreach (array('section', 'priority', 'version', 'filename', 'name') as $k)
+    if (!array_key_exists($k, $ini[$module]))
+      printf("warning: %s has no '%s' key.\n", $module, $k);
+}
+
+$header = "
+; Секции:
+;   core = необходимая функциональность
+;   base = базовая функциональность
+;   admin = функции для администрирования
+;   service = сервисные функции
+;   blog = работа с блогами
+;   spam = борьба со спамом
+;   commerce = электронная коммерция
+;   interaction = интерактив
+;   performance = производительность
+;   multimedia = мультимедийные функции
+;   syndication = обмен данными между сайтами
+;   templating = работа с шаблонами
+;   visual = визуальные редакторы
+";
+
+ini::write('modules.ini', $ini, $header);
