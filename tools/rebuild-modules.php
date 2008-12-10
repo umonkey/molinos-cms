b052478aecf55935d567aabcd04508de0bdd4c71
Justin Forest <justin.forest@gmail.com>
2008-12-10 15:39:37 +0300

    rebuild-modules.php использует класс zip.

--- tools/rebuild-modules.php
+++ tools/rebuild-modules.php
@@ -2,21 +2,6 @@
 
 require dirname(__FILE__) .'/../lib/bootstrap.php';
 
-function zip_folder($filename, $path)
-{
-  $path = rtrim($path, DIRECTORY_SEPARATOR);
-
-  printf("%s << %s\n", $filename, $path);
-
-  $z = new ZipArchive();
-  $z->open($filename, ZIPARCHIVE::OVERWRITE);
-
-  foreach (os::listFiles($path) as $file)
-    $z->addFile($file, substr($file, strlen($path) + 1));
-
-  $z->close();
-}
-
 if (!is_dir('modules'))
   mkdir('modules');
 
@@ -25,7 +10,7 @@ foreach (glob(os::path('lib', 'modules', '*', 'module.ini')) as $inifile) {
   $ini = ini::read($inifile);
   $zipname = $module . '-' . $ini['version'] . '.zip';
 
-  zip_folder(os::path('modules', $zipname), dirname($inifile));
+  zip::fromFolder(os::path('modules', $zipname), dirname($inifile));
 }
 
 printf("Done.\n");
