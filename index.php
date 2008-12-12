239f1a9a4c5e4dd719a4e873f2652189aabbc513
Justin Forest <justin.forest@gmail.com>
2008-12-12 21:28:59 +0300

    Заготовка для трассировки вызовов.
    Для включения нужно расскомментировать одну строку в index.php.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Расскоментировать для трассировки вызовов.
+// xdebug_start_trace(dirname($_SERVER['SCRIPT_FILENAME']) . DIRECTORY_SEPARATOR . "xdebug.log");
+
 if (version_compare(PHP_VERSION, "5.2.0", "<"))
   die('Для работы Molinos CMS требуется PHP версии 5.2.0, или более новый.');
 
