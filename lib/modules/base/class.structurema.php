7e1c79609261aa43f787d2b2093c3d2a63dc9caf
Justin Forest <justin.forest@gmail.com>
2008-12-16 17:32:38 +0300

    Управление роботами перенесено в базовую функциональность.

--- lib/modules/base/class.structurema.php
+++ lib/modules/base/class.structurema.php
@@ -97,7 +97,7 @@ class StructureMA
       'published' => $page->published,
       );
 
-    foreach (array('title', 'http_code', 'theme', 'lang', 'html_charset', 'params', 'defaultsection') as $k) {
+    foreach (array('title', 'http_code', 'theme', 'lang', 'html_charset', 'params', 'defaultsection', 'robots') as $k) {
       if (isset($page->$k))
         $data[$k] = $page->$k;
     }
