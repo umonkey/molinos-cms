3a506184d6ad8ae8889510029d9afac740f377e3
Justin Forest <justin.forest@gmail.com>
2008-12-16 17:01:25 +0300

    Модуль googleanalytics очищен от мусора.

---
+++ lib/modules/googleanalytics/class.googleanalyticsconfig.php
@@ -0,0 +1,30 @@
+<?php
+
+class GoogleAnalyticsConfig implements iModuleConfig
+{
+  public static function formGetModuleConfig()
+  {
+    $form = new Form(array(
+      'title' => t('Интеграция с Google Analytics'),
+      ));
+
+    $form->addControl(new TextLineControl(array(
+      'value' => 'config_account',
+      'label' => t('Учётная запись Google Analytics'),
+      'description' => t('Получить учётную запись можно на сайте <a href=\'@url\'>Google Analytics</a>, выглядит она примерно так: UA-123456-1.', array(
+        '@url' => 'http://www.google.com/analytics/',
+        )),
+      )));
+    $form->addControl(new BoolControl(array(
+      'value' => 'config_log_uids',
+      'label' => t('Передавать имена пользователей'),
+      'description' => t('При использовании этой опции Google будет получать имена залогиненных пользователей, что позволяет отделять их от анонимных.'),
+      )));
+
+    return $form;
+  }
+
+  public static function hookPostInstall()
+  {
+  }
+}
--- lib/modules/googleanalytics/class.googleanalyticsmodule.php
+++ lib/modules/googleanalytics/class.googleanalyticsmodule.php
@@ -1,76 +1,8 @@
 <?php
 // vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
 
-class GoogleAnalyticsModule implements iModuleConfig, iPageHook
+class GoogleAnalyticsModule implements iPageHook
 {
-  public static function formGetModuleConfig()
-  {
-    $form = new Form(array(
-      'title' => t('Интеграция с Google Analytics'),
-      ));
-
-    $form->addControl(new TextLineControl(array(
-      'value' => 'config_account',
-      'label' => t('Учётная запись Google Analytics'),
-      'description' => t('Получить учётную запись можно на сайте <a href=\'@url\'>Google Analytics</a>, выглядит она примерно так: UA-123456-1.', array(
-        '@url' => 'http://www.google.com/analytics/',
-        )),
-      )));
-    $form->addControl(new BoolControl(array(
-      'value' => 'config_log_uids',
-      'label' => t('Передавать имена пользователей'),
-      'description' => t('При использовании этой опции Google будет получать имена залогиненных пользователей, что позволяет отделять их от анонимных.'),
-      )));
-
-    return $form;
-  }
-
-  public static function hookPostInstall()
-  {
-    $t = new TableInfo('node__log');
-
-    $t->columnSet('lid', array(
-      'type' => 'int',
-      'required' => true,
-      'key' => 'pri',
-      'autoincrement' => true,
-      ));
-    $t->columnSet('timestamp', array(
-      'type' => 'datetime',
-      'key' => 'mul',
-      'required' => true,
-      ));
-    $t->columnSet('nid', array(
-      'type' => 'int',
-      'required' => false,
-      'key' => 'mul',
-      ));
-    $t->columnSet('uid', array(
-      'type' => 'int',
-      'required' => false,
-      'key' => 'mul',
-      ));
-    $t->columnSet('username', array(
-      'type' => 'varchar(255)',
-      'required' => false,
-      'key' => 'mul',
-      ));
-    $t->columnSet('ip', array(
-      'type' => 'varchar(15)',
-      'required' => true,
-      'key' => 'mul',
-      ));
-    $t->columnSet('operation', array(
-      'type' => 'varchar(10)',
-      'key' => 'mul',
-      ));
-    $t->columnSet('message', array(
-      'type' => 'TEXT',
-      ));
-
-    $t->commit();
-  }
-
   public static function hookPage(&$output, Node $page)
   {
     $config = mcms::modconf('googleanalytics');
@@ -81,7 +13,7 @@ class GoogleAnalyticsModule implements iModuleConfig, iPageHook
       $html .= "_uacct = '{$config['account']}';";
 
       if (!empty($config['log_uids']))
-        $html .= "__utmSetVar('". mcms_plain(mcms::user()->login) ."');";
+        $html .= "__utmSetVar('". mcms_plain(mcms::user()->getName()) ."');";
 
       $html .= "urchinTracker();";
       $html .= '</script>';
