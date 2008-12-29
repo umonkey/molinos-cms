82355ab66f4d93c034e738a8c36bdd5b7b78313b
Justin Forest <justin.forest@gmail.com>
2008-12-29 16:59:39 +0300

    Поправлен модуль maintenance.
    1. Работает меню.
    2. Работает "запирание" сайта (не вываливается в ISE).

--- lib/modules/maintenance/class.maintenancemodule.php
+++ lib/modules/maintenance/class.maintenancemodule.php
@@ -1,7 +1,7 @@
 <?php
 // vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
 
-class MaintenanceModule implements iModuleConfig
+class MaintenanceModule implements iRequestHook
 {
   public static function hookRequest(Context $ctx = null)
   {
