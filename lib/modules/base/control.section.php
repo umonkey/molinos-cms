ff170680ac26b3728c6557a99b56e254485a1724
Justin Forest <justin.forest@gmail.com>
2009-02-06 12:52:02 +0300

    Поправлена привязка документа к разделу.
    Перед установкой нового раздела отвязываем от старых.

--- lib/modules/base/control.section.php
+++ lib/modules/base/control.section.php
@@ -34,6 +34,7 @@ class SectionControl extends EnumControl implements iFormControl
     if ($this->store)
       $node->{$this->value} = $value;
     else {
+      $node->onsave("DELETE FROM `node__rel` WHERE `nid` = %ID% AND `key` IS NULL AND `tid` IN (SELECT `id` FROM `node` WHERE `class` = 'tag')");
       $params = array();
       $node->onSave("INSERT INTO `node__rel` (`nid`, `tid`) SELECT %ID%, `id` FROM `node` WHERE `class` = 'tag' AND `id` " . sql::in($value, $params), $params);
     }
