f59178f1c40c5cd9c0134e3a4b536ca4e7171a6f
Justin Forest <justin.forest@gmail.com>
2009-02-09 18:39:52 +0300

    Обновлена структура БД, добавлен тестовый конфиг.

---
+++ conf/ru.molinos.cms.test.config.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'mail.from' => 'no-reply@molinos.ru',
+  'mail.server' => 'localhost',
+  'backtracerecipients' => array (
+    'cms-bugs@molinos.ru',
+  ),
+  'filestorage' => 'storage',
+  'ftpfolder' => 'ftp',
+  'tmpdir' => 'tmp',
+  'file.cache.ttl' => '3600',
+  'db.default' => 'sqlite:conf/test.db',
+);
