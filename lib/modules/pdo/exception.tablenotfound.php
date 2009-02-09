a3d107dbf6992c9ab2567ac855fc83335c23d1be
Justin Forest <justin.forest@gmail.com>
2009-02-09 20:08:53 +0300

    TableNotFoundException не ходит за контекстом.

--- lib/modules/pdo/exception.tablenotfound.php
+++ lib/modules/pdo/exception.tablenotfound.php
@@ -3,14 +3,10 @@
 
 class TableNotFoundException extends Exception
 {
-  private $sql = null;
-  private $params = null;
   private $table = null;
 
-  public function __construct($table, $sql = null, $params = null)
+  public function __construct($table)
   {
-    $this->sql = $sql;
-    $this->params = $params;
     $this->table = $table;
 
     switch ($table) {
@@ -20,20 +16,6 @@ class TableNotFoundException extends Exception
     }
 
     parent::__construct("Таблица {$table} не найдена.");
-  }
-
-  public function getQuery()
-  {
-    return (($ctx = Context::last()) and $ctx->canDebug())
-      ? $this->sql
-      : null;
-  }
-
-  public function getParams()
-  {
-    return (($ctx = Context::last()) and $ctx->canDebug())
-      ? $this->params
-      : null;
   }
 
   public function getTableName()
--- lib/modules/rating/class.ratingcleaner.php
+++ lib/modules/rating/class.ratingcleaner.php
@@ -5,7 +5,7 @@ class RatingCleaner implements iNodeHook
   public static function hookNodeUpdate(Node $node, $op)
   {
     if ($op == 'erase') {
-      $db = Context::last()->db;
+      $db = $node->getDB();
       if ('user' == $node->class)
         $db->exec("DELETE FROM `node_rating` WHERE `nid` = :nid OR `uid` = :uid", array(':nid' => $node->id, ':uid' => $node->id));
       else
