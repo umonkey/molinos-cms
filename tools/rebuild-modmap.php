f721e4bf4cec90f114ff0a2a25dcc17a92db3d0c
Justin Forest <justin.forest@gmail.com>
2009-02-12 15:08:41 +0300

    Наведён порядок в папке toold.

--- tools/rebuild-modmap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require dirname(__FILE__) .'/../lib/bootstrap.php';
-
-Loader::rebuild($local = !file_exists($conf = 'conf' . DIRECTORY_SEPARATOR . 'default.config.php'));
-
-if ($local and file_exists($conf))
-  unlink($conf);
