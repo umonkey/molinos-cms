9a6de2dfedbbbc2c79bf1acd87a95f1f489a0142
Justin Forest <justin.forest@gmail.com>
2009-02-13 18:48:20 +0300

    v8.12.3

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Текущая версия.
-define('MCMS_VERSION', '8.12.2');
+define('MCMS_VERSION', '8.12.3');
 
 // Начало обработки запроса, для замеров производительности.
 define('MCMS_START_TIME', microtime(true));
