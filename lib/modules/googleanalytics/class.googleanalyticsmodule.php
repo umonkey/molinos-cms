a6b356b337096e57d5e37f597645dbc0a7ca756c
Justin Forest <justin.forest@gmail.com>
2009-03-05 05:49:56 +0300

    Поправлен модуль googleanalytics.

--- lib/modules/googleanalytics/class.googleanalyticsmodule.php
+++ lib/modules/googleanalytics/class.googleanalyticsmodule.php
@@ -1,24 +1,19 @@
 <?php
 // vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2:
 
-class GoogleAnalyticsModule implements iPageHook
+class GoogleAnalyticsModule implements iRequestHook
 {
-  public static function hookPage(&$output, Node $page)
+  public static function hookRequest(Context $ctx = null)
   {
-    $config = mcms::modconf('googleanalytics');
-
-    if (!empty($config['account'])) {
-      $html = '<script type=\'text/javascript\' src=\'http://www.google-analytics.com/urchin.js\'></script>';
-      $html .= "<script type='text/javascript'>";
-      $html .= "_uacct = '{$config['account']}';";
-
-      if (!empty($config['log_uids']))
-        $html .= "__utmSetVar('". mcms_plain(Context::last()->user->getName()) ."');";
-
-      $html .= "urchinTracker();";
-      $html .= '</script>';
-
-      $output = str_replace('</head>', $html .'</head>', $output);
+    $conf = mcms::modconf('googleanalytics', 'config');
+    if (!empty($conf['account'])) {
+      $script = 'var gaJsHost = (("https:" == document.location.protocol) '
+        . '? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src=\'" + gaJsHost + '
+        . '"google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));';
+      $ctx->addExtra('script', $script);
+      $script = 'try{var pageTracker = _gat._getTracker("' . $conf['account']
+        . '");pageTracker._trackPageview();}catch(err){}';
+      $ctx->addExtra('script', $script);
     }
   }
 };
