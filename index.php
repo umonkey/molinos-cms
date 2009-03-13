8c1dee7a5fb20336e587a041a9789783f1d5f810
Justin Forest <justin.forest@gmail.com>
2009-03-13 17:28:40 +0300

    Упрощено кэширование кода.
    В начале обработки запроса считывается файл /codecache.php,
    который может содержать часто используемые классы, для предотвращения
    автозагрузки.  Эффективно для небольших классов или при использовании
    акселераторов.

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
+@include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'codecache.php';
+
 // Для включения локального лога расскомментировать:
 // ini_set('error_log', dirname($_SERVER['SCRIPT_FILENAME']) . DIRECTORY_SEPARATOR . 'php.log');
 
 // Расскоментировать для трассировки вызовов.
 // xdebug_start_trace(dirname($_SERVER['SCRIPT_FILENAME']) . DIRECTORY_SEPARATOR . "xdebug.log");
 
-if (version_compare(PHP_VERSION, "5.2.0", "<"))
+if (version_compare(PHP_VERSION, "5.2.0", "<")) {
+  header('Content-Type: text/plain; charset=utf-8');
   die('Для работы Molinos CMS требуется PHP версии 5.2.0, или более новый.');
+}
 
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'bootstrap.php');
+// Основной загрузчик. Инициализирует реестр, запускает обработку запроса.
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR
+  . 'modules' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'class.loader.php';
 
-mcms::run();
+Loader::run();
