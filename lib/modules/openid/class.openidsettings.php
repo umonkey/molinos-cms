e23ab65324f38bb11f8d303baf863cbd01ac7a36
Justin Forest <justin.forest@gmail.com>
2009-03-17 18:12:52 +0300

    Поправлены настройки модуля OpenID.
    @ver openid 9.03.2

--- lib/modules/openid/class.openidsettings.php
+++ lib/modules/openid/class.openidsettings.php
@@ -7,66 +7,33 @@ class OpenIdSettings
    */
   public static function formGetModuleConfig()
   {
-    $form = new Form(array(
-      'title' => t('Настройка авторизации'),
-      'class' => 'tabbed',
-      ));
-
-    $tab = new FieldSetControl(array(
-      'name' => 'tab_openid',
-      'label' => t('OpenID'),
-      ));
-    $tab->addControl(new EnumRadioControl(array(
-      'value' => 'config_mode',
-      'label' => t('Режим работы'),
-      'options' => array(
-        'open' => t('<strong>С прозрачной регистрацией</strong><br/>Максимальный комфорт для пользователя, регистрироваться в явном виде не нужно.'),
-        'closed' => t('<strong>С ручной регистрацией</strong><br/>Использовать <a href=\'@url\'>OpenID</a> для авторизации, но добавлением пользователей в систему занимается администратор сайта.', array('@url' => 'http://ru.wikipedia.org/wiki/OpenID')),
-        'none' => t('<strong>Отключить</strong><br/>Не понятно, кому и зачем этот режим может понадобиться, но пусть будет.'),
+    return new Schema(array(
+      'mode' => array(
+        'type' => 'EnumRadioControl',
+        'label' => t('Режим работы'),
+        'options' => array(
+          'open' => t('Прозрачная регистрация'),
+          'closed' => t('Только авторизация, без регистрации'),
+          ),
+        'required' => true,
+        'default' => 'open',
         ),
-      'required' => true,
-      'default' => 'open',
-      )));
-    $form->addControl($tab);
-
-    $tab = new FieldSetControl(array(
-      'name' => 'tab_profilefields',
-      'label' => t('Профиль'),
-      ));
-    $tab->addControl(new SetControl(array(
-      'value' => 'config_profile_fields',
-      'label' => t('Запрашиваемые атрибуты'),
-      'options' => array(
-        'email' => 'Email',
-        'fullname' => 'Полное имя',
-        'dob' => 'Дата рождения',
-        'gender' => 'Пол',
-        'postcode' => 'Почтовый индекс',
-        'country' => 'Страна',
-        'language' => 'Предпочтительный язык',
-        'timezone' => 'Часовой пояс',
+      'profile_fields' => array(
+        'type' => 'SetControl',
+        'label' => t('Запрашиваемые атрибуты'),
+        'options' => array(
+          'email' => 'Email',
+          'fullname' => 'Полное имя',
+          'dob' => 'Дата рождения',
+          'gender' => 'Пол',
+          'postcode' => 'Почтовый индекс',
+          'country' => 'Страна',
+          'language' => 'Предпочтительный язык',
+          'timezone' => 'Часовой пояс',
+          ),
+        'description' => t('Поля приведены в соответствии со <a href=\'@url\'>спецификацией</a>.', array('@url' => 'http://openid.net/specs/openid-simple-registration-extension-1_0.html#response_format')),
         ),
-      'description' => t('Поля приведены в соответствии со <a href=\'@url\'>спецификацией</a>.', array('@url' => 'http://openid.net/specs/openid-simple-registration-extension-1_0.html#response_format')),
-      )));
-    $form->addControl($tab);
-
-    $tab = new FieldSetControl(array(
-      'name' => 'tab_groups',
-      'label' => t('Доступ'),
       ));
-    $tab->addControl(new SetControl(array(
-      'value' => 'config_groups_anon',
-      'label' => t('Группы для анонимных посетителей'),
-      'options' => self::getGroups(),
-      )));
-    $tab->addControl(new SetControl(array(
-      'value' => 'config_groups',
-      'label' => t('При регистрации помещать в'),
-      'options' => self::getGroups(),
-      )));
-    $form->addControl($tab);
-
-    return $form;
   }
 
   private static function getGroups()
