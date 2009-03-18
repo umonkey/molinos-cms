7cdd0b770acc56bf6d183cd6364b31bb6fdebcac
Justin Forest <justin.forest@gmail.com>
2009-03-18 16:05:59 +0300

    Автоматическое добавление типа subscription.
    @ver subscription 9.03.6

--- lib/modules/subscription/class.subscriptionadmin.php
+++ lib/modules/subscription/class.subscriptionadmin.php
@@ -17,4 +17,27 @@ class SubscriptionAdmin
         'description' => t('Управление подпиской на новости.'),
         );
   }
+
+  /**
+   * Инсталляция типа документа subscription.
+   *
+   * @mcms_message ru.molinos.cms.install
+   */
+  public static function onInstall(Context $ctx)
+  {
+    try {
+      $node = Node::load(array(
+        'class' => 'type',
+        'name' => 'subscription',
+        'deleted' => 0,
+        ), $ctx->db);
+    } catch (ObjectNotFoundException $e) {
+      $ctx->db->beginTransaction();
+      $node = Node::create('type', array(
+        'name' => 'subscription',
+        'title' => t('Подписка на новости'),
+        ))->save();
+      $ctx->db->commit();
+    }
+  }
 }
