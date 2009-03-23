9df514f6826d606c22a37d29e898e52057274121
Justin Forest <justin.forest@gmail.com>
2009-03-23 12:16:08 +0300

    Виджет для получения содержимого справочника.
    @ver base 9.03.18

---
+++ lib/modules/base/widget.dict.php
@@ -0,0 +1,45 @@
+<?php
+
+class DictWidget extends Widget
+{
+  /**
+   * Возвращает описание виджета.
+   *
+   * @return array массив с описанием виджета, ключи: name, description.
+   * @mcms_message ru.molinos.cms.widget.enum
+   */
+  public static function getWidgetInfo()
+  {
+    return array(
+      'name' => 'Содержимое справочника',
+      'description' => 'Выводит все элементы справочника, например, для построения навигационных блоков или параметрического поиска.',
+      );
+  }
+
+  public static function getConfigOptions()
+  {
+    $list = TypeNode::getDictionaries();
+
+    if (empty($list))
+      throw new ForbiddenException(t('Не определён ни один справочник.'));
+
+    return array(
+      'type' => array(
+        'type' => 'EnumControl',
+        'label' => t('Справочник'),
+        'required' => true,
+        'options' => $list,
+        ),
+      );
+  }
+
+  public function onGet(array $options)
+  {
+    return Node::findXML($this->ctx->db, array(
+      'class' => $this->type,
+      'deleted' => 0,
+      'published' => 1,
+      '#sort' => 'name',
+      ));
+  }
+}
