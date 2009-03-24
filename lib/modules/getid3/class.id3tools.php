11dc563dbdf673293bdf000893abc26caa7ac81d
Justin Forest <justin.forest@gmail.com>
2009-03-24 20:23:03 +0300

    Поправлено определение размера флэшек.
    http://code.google.com/p/molinos-cms/issues/detail?id=761

--- lib/modules/getid3/class.id3tools.php
+++ lib/modules/getid3/class.id3tools.php
@@ -7,14 +7,10 @@ class ID3Tools
     if (is_numeric($node))
       $node = Node::load($node);
 
-    if (is_object($node))
-      $node = $node->getRaw();
-
     require_once dirname(__FILE__) .'/getid3/getid3.php';
 
     $getID3 = new getID3();
-    $info = $getID3->analyze(mcms::config('filestorage')
-      .'/'. $node['filepath']);
+    $info = $getID3->analyze($node->getRealURL());
 
     return array(
       $info['video']['resolution_x'],
