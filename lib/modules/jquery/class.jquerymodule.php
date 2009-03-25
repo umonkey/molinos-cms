7f87a0c29c3a94ba883798ac77b9d742fd9b5cd7
Justin Forest <justin.forest@gmail.com>
2009-03-25 16:39:50 +0300

    Новый модуль: jquery.
    @ver jquery 9.03.1

---
+++ lib/modules/jquery/class.jquerymodule.php
@@ -0,0 +1,41 @@
+<?php
+
+class jQueryModule
+{
+  /**
+   * @mcms_message ru.molinos.cms.compressor.enum
+   */
+  public static function on_compressor_enum(Context $ctx)
+  {
+    $libs = array();
+
+    if ($version = $ctx->modconf('jquery', 'jsversion', '1.2.6'))
+      $libs[] = array('script', os::path('lib', 'modules', 'jquery', 'jquery-' . $version . '.min.js'));
+
+    if ($ctx->modconf('jquery', 'ui'))
+      $libs[] = array('script', os::path('lib', 'modules', 'jquery', 'jquery-ui-1.5.3.min.js'));
+
+    return $libs;
+  }
+
+  /**
+   * @mcms_message ru.molinos.cms.admin.config.module.jquery
+   */
+  public static function formGetModuleConfig()
+  {
+    return new Schema(array(
+      'jsversion' => array(
+        'type' => 'EnumControl',
+        'label' => t('Версия jQuery'),
+        'options' => array(
+          '1.2.6',
+          ),
+        'required' => true,
+        ),
+      'ui' => array(
+        'type' => 'BoolControl',
+        'label' => t('Подключить jQuery UI'),
+        ),
+      ));
+  }
+}
