a2b24de85e9c9b5198bd3f53a7cc902d6a870281
Justin Forest <justin.forest@gmail.com>
2009-04-10 15:29:29 +0400

    Molinos CMS v8.12.4

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Текущая версия.
-define('MCMS_VERSION', '8.12.3');
+define('MCMS_VERSION', '8.12.4');
 
 // Начало обработки запроса, для замеров производительности.
 define('MCMS_START_TIME', microtime(true));
