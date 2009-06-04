1c4920ff382dd981a318742b4dabed49b1c7bd40
Justin Forest <justin.forest@gmail.com>
2009-06-04 19:32:14 +0400

    api/labels/suggest.xml
    labels v9.05.9

---
+++ lib/modules/labels/class.labelsapi.php
@@ -0,0 +1,30 @@
+<?php
+
+class LabelsAPI
+{
+  public static function on_find_xml(Context $ctx)
+  {
+    if (null === ($search = $ctx->get('search')))
+      throw new BadRequestException(t('Не указана подстрока для поиска (GET-параметр search).'));
+
+    if (!($limit = intval($ctx->get('limit', 5))))
+      throw new BadRequestException(t('Не указано количество возовращаемых меток (GET-параметр limit).'));
+
+    list($sql, $params) = Query::build(array(
+      'class' => 'label',
+      'deleted' => 0,
+      'published' => 1,
+      'name?|' => '%' . $search . '%',
+      '#sort' => 'name',
+      ))->getSelect(null, null, 'name');
+
+    $result = '';
+    foreach ($ctx->db->getResultsV("name", $sql, $params) as $name)
+      $result .= html::em('label', html::cdata($name));
+
+    return new Response(html::em('labels', array(
+      'search' => $search,
+      'limit' => $limit,
+      ), $result), 'text/xml');
+  }
+}
