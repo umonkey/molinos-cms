8a32282e2be1c0d8e6295002adacbecddd5aac82
Justin Forest <justin.forest@gmail.com>
2009-06-09 18:45:20 +0400

    Вывод результатов голосования через XML API.
    api/poll/results.xml?id=
    
    poll v9.05.3

--- lib/modules/poll/class.pollrpc.php
+++ lib/modules/poll/class.pollrpc.php
@@ -33,4 +33,27 @@ class PollRPC
 
     return $ctx->getRedirect();
   }
+
+  /**
+   * Возвращает результаты по опросу.
+   */
+  public static function on_get_results(Context $ctx)
+  {
+    $output = '';
+
+    $id = $ctx->get('id');
+    $data = $ctx->db->getResultsKV("option", "count", "SELECT `option`, COUNT(*) AS `count` FROM `node__poll` WHERE `nid` = ? GROUP BY `option`", array($ctx->get('id')));
+    foreach ($data as $k => $v)
+      $output .= html::em('option', array(
+        'count' => $v,
+        ), html::cdata($k));
+
+    $voted = $ctx->user->id
+      ? $ctx->db->fetch("SELECT COUNT(*) FROM `node__poll` WHERE `nid` = ? AND `uid` = ?", array($id, $ctx->user->id))
+      : $ctx->db->fetch("SELECT COUNT(*) FROM `node__poll` WHERE `nid` = ? AND `ip` = ?", array($id, $_SERVER['REMOTE_ADDR']));
+
+    return new Response(html::em('results', array(
+      'voted' => (bool)$voted,
+      ), $output), 'text/xml');
+  }
 }
