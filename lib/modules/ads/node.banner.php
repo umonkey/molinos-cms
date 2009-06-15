51e2c08cd81f9acc41dd19fe48abbdd7623e2a7d
Justin Forest <justin.forest@gmail.com>
2009-06-15 18:31:51 +0400

    Доработан вывод баннеров.
    При редактировании количества показов счётчик не перезапускается.
    
    ads v9.05.2

--- lib/modules/ads/node.banner.php
+++ lib/modules/ads/node.banner.php
@@ -2,11 +2,12 @@
 
 class BannerNode extends Node
 {
-  const table = 'node__banners';
-
   public function save()
   {
-    $this->onSave("REPLACE INTO `" . self::table . "` (`id`, `time_limit`, `display_count`) VALUES (%ID%, ?, ?)", array($this->time_limit, $this->display_count));
+    if ($this->isNew())
+      $this->onSave("INSERT INTO `node__banners` (`id`, `time_limit`, `display_limit`) VALUES (%ID%, ?, ?)", array($this->time_limit, $this->display_limit));
+    else
+      $this->onSave("UPDATE `node__banners` SET `time_limit` = ?, `display_limit` = ? WHERE `id` = %ID%", array($this->time_limit, $this->display_limit));
     return parent::save();
   }
 
@@ -21,7 +22,7 @@ class BannerNode extends Node
       'weight' => 100,
       'group' => t('Управление показами'),
       );
-    $schema['display_count'] = array(
+    $schema['display_limit'] = array(
       'type' => 'NumberControl',
       'label' => t('Количество показов'),
       'required' => false,
@@ -38,7 +39,7 @@ class BannerNode extends Node
    */
   public static function on_install(Context $ctx)
   {
-    TableInfo::check(self::table, array(
+    TableInfo::check('node__banners', array(
       'id' => array(
         'type' => 'integer',
         'key' => 'pri',
@@ -48,6 +49,10 @@ class BannerNode extends Node
         'key' => 'mul',
         ),
       'display_count' => array(
+        'type' => 'integer',
+        'key' => 'mul',
+        ),
+      'display_limit' => array(
         'type' => 'integer',
         'key' => 'mul',
         ),
@@ -68,16 +73,26 @@ class BannerNode extends Node
   }
 
   /**
+   * Вызов api/ads/get.xml
+   *
    * Возвращает случайные баннеры.
    */
   public static function on_get_banners(Context $ctx)
   {
     $limit = intval($ctx->get('limit', 1));
-    $data = $ctx->db->getResultsKV("id", "xml", "SELECT `id`, `xml` FROM `node` WHERE `class` = 'banner' AND `deleted` = 0 AND `published` = 1 AND `id` IN (SELECT `id` FROM `" . self::table . "` WHERE (`time_limit` IS NULL OR `time_limit` < ?) AND `display_count` <> 0) ORDER BY RAND() LIMIT {$limit}", array(mcms::now()));
+
+    $sql = "SELECT `node`.`id`, `xml` FROM `node` "
+      . "INNER JOIN `node__banners` ON `node__banners`.`id` = `node`.`id` "
+      . "WHERE `class` = 'banner' AND `deleted` = 0 AND `published` = 1 "
+      . "AND (`time_limit` IS NULL OR `time_limit` < ?) "
+      . "AND (`display_limit` IS NULL OR `display_count` IS NULL OR `display_count` < `display_limit`) "
+      . "ORDER BY RAND() LIMIT " . $limit;
+    $data = $ctx->db->getResultsKV("id", "xml", $sql, array(mcms::now()));
 
     $params = array();
     $ctx->db->beginTransaction();
-    $ctx->db->exec("UPDATE `" . self::table . "` SET `display_count` = `display_count` - 1 WHERE `display_count` IS NOT NULL AND `id` " . sql::in(array_keys($data), $params), $params);
+    $ctx->db->exec("UPDATE `node__banners` SET `display_count` = 0 WHERE `display_count` IS NULL");
+    $ctx->db->exec("UPDATE `node__banners` SET `display_count` = `display_count` + 1 WHERE `id` " . sql::in(array_keys($data), $params), $params);
     $ctx->db->commit();
 
     return new Response(html::em('nodes', implode('', $data)), 'text/xml');
