bd395c54b8a7cc38eea11296dd7c8abf8994470e
Justin Forest <justin.forest@gmail.com>
2009-06-19 18:44:41 +0400

    Исправлена работа с файловым архивом в TinyMCE.
    tinymce v9.05.4

--- lib/modules/tinymce/class.tinymcemodule.php
+++ lib/modules/tinymce/class.tinymcemodule.php
@@ -6,7 +6,7 @@ class TinyMceModule
   /**
    * Возвращает список скриптов и стилей для использования на сайте.
    *
-   * @mcms_message ru.molinos.cms.compressor.enum
+   * @mcms_message ru.molinos.cms.compressor.enumXXX
    */
   public static function on_compressor_enum(Context $ctx, $mode = 'website')
   {
@@ -46,10 +46,41 @@ class TinyMceModule
   }
 
   /*
-   * @mcms_message ru.molinos.cms.compressor.enum.admin
+   * @mcms_message ru.molinos.cms.compressor.enum.adminXXX
    */
   public static function on_compressor_admin(Context $ctx)
   {
     return self::on_compressor_enum($ctx, 'admin');
+  }
+
+  /**
+   * @mcms_message ru.molinos.cms.page.head
+   */
+  public static function on_get_head(Context $ctx)
+  {
+    $result = '';
+
+    $base = $ctx->url()->getBase($ctx);
+    $result .= html::em('script', array(
+      'type' => 'text/javascript',
+      ), "var mcms_url = '{$base}';");
+
+    if ($picker = $ctx->get('tinymcepicker')) {
+      $result .= html::em('script', "var tinyMcePicker = '{$picker}';");
+      $result .= html::em('script', array(
+        'src' => $base . 'lib/modules/tinymce/editor/tiny_mce_popup.js',
+        'type' => 'text/javascript',
+        ));
+    } elseif (preg_match('%^(admin/edit/|admin/create/)%', $ctx->query())) {
+      foreach (self::on_compressor_admin($ctx) as $script)
+        $result .= html::em('script', array(
+          'src' => $base . $script[1],
+          'type' => 'text/javascript',
+          ));
+    }
+
+    return html::wrap('head', html::cdata($result), array(
+      'module' => 'tinymce',
+      ));
   }
 }
