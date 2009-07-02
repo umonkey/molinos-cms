6f5d2a758c7ed337bcd70c83dfe0cba53b1ff341
Justin Forest <justin.forest@gmail.com>
2009-07-02 15:33:06 +0400

    Запуск крона из консоли снова поддерживает указание доменного имени.

--- tools/cron.php
+++ tools/cron.php
@@ -5,7 +5,7 @@ if (empty($argv[1])) {
   exit(1);
 }
 
-define('MCMS_HOST', $argv[1]);
+define('MCMS_HOST_NAME', $argv[1]);
 define('MCMS_REQUEST_URI', '?q=cron.rpc');
 
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'index.php';
